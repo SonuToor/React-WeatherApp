b8823179ba3ca54f60837b00ca30fe790428eb02
SonuToor <savrooptoor92@gmail.com>
2020-01-18 02:06:37 -0500

    got the weather-icons (the better looking icons) to display.

--- src/components/weatherdisplay/WeatherTile.js
+++ src/components/weatherdisplay/WeatherTile.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
-import { capitalizeString, militaryTimeToStandard } from "../../utils";
+import { capitalizeString, militaryTimeToStandard, returnIconClassName } from "../../utils";
+import  "../../../node_modules/weather-icons/css/weather-icons.css"
 
 const StyledTile = styled.div`
   display: flex;
@@ -21,14 +22,28 @@ const MainInfo = styled.div`
   text-align: center;
 `
 
+// TO DO 
+  // tranform size 1.5x when tile is hovered over
+const WeatherIcon = styled.i`
+
+
+`
+
+// TO DO 
+  // dayOrNight seem to be giving me inconsistent results for when m or 
+  // should be. Isolate dayOrNight try using the debugger and figure it out.
+
 
 const WeatherTile = ({ data }) => {
   let temp = `${data.main.temp}°C`
   let time = militaryTimeToStandard(data["dt_txt"].slice(10, -3))
   let weather = capitalizeString(data.weather[0].description)
-  let icon = data.weather[0].icon
+  let code = data.weather[0].id
+  let dayOrNight = data.sys.pod
+
+  console.log(code, dayOrNight)
+  console.log(returnIconClassName(code, dayOrNight))
   const [showDetail, toggleShowDetails] = useState(false)
-  let iconSrc = `http://openweathermap.org/img/w/${icon}.png`
 
   const showLess = () => {
     toggleShowDetails(false)
@@ -42,7 +57,11 @@ const WeatherTile = ({ data }) => {
           <h3>{time}</h3>
           <p>{temp}</p>
         </MainInfo>
-        <img src={iconSrc} alt="weather image"/>
+        {/* <p>{returnIconClassName(code, dayOrNight)}</p> */}
+        {/* <p>{code}{dayOrNight}</p> */}
+        <WeatherIcon>
+          <i className={returnIconClassName(code, dayOrNight)}></i>
+        </WeatherIcon>
         {showDetail ? <p>{weather}</p> : null }
       </StyledTile>    
     )
